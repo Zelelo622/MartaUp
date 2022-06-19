4c49d9dc932e796a20f8440502ce2d41510a45e5
Egor Kirin <egorcom27@gmail.com>
2022-06-19 21:42:01 +0300

    header block

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.header__slider').slick({
+        vertical: true,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        dotsClass: 'header-dots',
+    });
+})
